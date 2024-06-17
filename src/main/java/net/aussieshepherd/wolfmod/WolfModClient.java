82f3ba87afb53aea99b2fe0f9062896fa9b489a3
aussieshepherd <beargarbailey@gmail.com>
2024-06-17 13:38:54 -0700

    Commit

---
+++ src/main/java/net/aussieshepherd/wolfmod/WolfModClient.java
@@ -0,0 +1,10 @@
+package net.aussieshepherd.wolfmod;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class WolfModClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
